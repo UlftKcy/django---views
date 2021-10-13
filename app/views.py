51110f4aa3aebe34eaaa06e00a71a1d642f0e549
UlfetKacay <ukacay87@gmail.com>
2021-10-13 22:53:27 +0300

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# from django.http import HttpResponse
+
+def home(request):
+    context = {
+        'title' : 'clarusway',
+        'dict1' : {'django': 'best framework'},
+        'my_list' : [1,2,3],
+    }
+    return render(request,'app/home.html',context)
+
